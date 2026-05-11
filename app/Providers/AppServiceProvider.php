4b136eb3b65109104df30df1a84f91fb223a1e2c
Ahemad <ahemadraza887@gmail.com>
2026-05-11 10:15:31 +0530

    migration error solved and images added

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,29 +2,32 @@
 namespace App\Providers;
 
 use App\Models\MemorialGuide;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        View::share(
-            'memorialGuideMenu',
-            MemorialGuide::where('is_active', true)
-                ->orderBy('order')
-                ->get()
-        );
+        if (Schema::hasTable('memorial_guides')) {
+
+            View::share(
+                'memorialGuideMenu',
+                MemorialGuide::where('is_active', true)
+                    ->orderBy('order')
+                    ->get()
+            );
+
+        } else {
+
+            View::share('memorialGuideMenu', collect());
+
+        }
     }
 }
