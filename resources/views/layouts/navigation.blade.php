2c0499e63c10a5e657ea21e23753dc9110e08c79
Ahemad <ahemadraza887@gmail.com>
2026-05-12 19:47:56 +0530

    Replace application logo component with image

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,9 +5,13 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
-                    </a>
+                    <a href="/">
+                    <img
+                        src="{{ asset('images/logo.png') }}"
+                        alt="Dietz Memorial"
+                        class="w-20 h-auto"
+                    >
+                </a>
                 </div>
 
                 <!-- Navigation Links -->
